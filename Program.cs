3a6cb73969b76082a440f8239e9d65692537d09d
Iván Sainz (SXO) <ivan.sainz@aheadlabs.com>
2018-11-13 17:27:49 +0100

    Scaffolding

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace daysdifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
